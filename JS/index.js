26794c48d07b3a649f55d2bddc2517f8143b9654
masaya7 <52489219+masaya7@users.noreply.github.com>
2023-07-21 16:27:24 +0900

    maybe_tap_ok

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,9 @@
-$(function() {
-  $('.slider').slick();
+$(function () {
+  $(".slider").slick({
+    autoplay: true,
+    adaptiveHeight: true,
+    swipe: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>'
+  });
 });
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Flicking
+
+### カスタマイズするときに参照
+https://junpei-sugiyama.com/slick-option/
